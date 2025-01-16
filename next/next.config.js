d0e53a1fa237d39ac943f9c372f56629d0840601
manato-takahashi <manato.takahashi86@gmail.com>
2025-01-16 20:29:58 +0900

    add: Next.jsの仕組み（基礎）を完了

--- next/next.config.js
+++ next/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 300,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
